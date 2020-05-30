e6b477295c51861211bd902ad41e61483e2fa472
Bearbrice <berclazpro@gmail.com>
2020-05-30 09:12:43 +0200

    Hall of fame added

--- menu.js
+++ menu.js
@@ -1,19 +1,21 @@
 window.onload = function(){
     document.getElementById("play").style.display = "none";
     document.getElementById("rules").style.display = "none";
-    document.getElementById("hallfame").style.display = "none";
+    document.getElementById("hallOfFame").style.display = "none";
 
     //Declare the buttons
     var rulesButton = document.getElementById("rulesButton");
     var okButton = document.getElementById("ok");
     var playButton = document.getElementById("playButton");
     var cancelButton = document.getElementById("cancel");
+    var hofButton = document.getElementById("hofButton");
 
     //Add event listener to the buttons
     rulesButton.addEventListener("click", rulesClick);
     okButton.addEventListener("click", okClick);
     playButton.addEventListener("click", playClick);
     cancelButton.addEventListener("click", cancelClick);
+    hofButton.addEventListener("click", hofClick);
 
     //Show the rules and hide the menu
     function rulesClick(){
@@ -25,6 +27,7 @@ window.onload = function(){
     function okClick(){
         document.getElementById("menu").style.display = "";
         document.getElementById("rules").style.display = "none";
+        document.getElementById("hallOfFame").style.display = "none";
     }
 
     //Show the choice of level and hide the menu
@@ -37,5 +40,11 @@ window.onload = function(){
     function cancelClick(){
         document.getElementById("menu").style.display = "";
         document.getElementById("play").style.display = "none";
+    }
+
+    //Show the hall of fame
+    function hofClick(){
+        document.getElementById("hallOfFame").style.display = "";
+        document.getElementById("menu").style.display = "none";
     }
 }
