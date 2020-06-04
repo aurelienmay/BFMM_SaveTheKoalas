3f666625390b4b7279442cbf1536e76cb5620a41
aurélien <aurelienmay98@gmail.com>
2020-06-04 08:49:13 +0200

    Border.

--- game.js
+++ game.js
@@ -456,7 +456,7 @@ bushImage.src = "ressources/images/decor/bush_50.png";
 
 // Border image
 var borderImage = new Image();
-borderImage.src = "ressources/images/decor/tress.png";
+borderImage.src = "ressources/images/decor/tresss.png";
 
 // Safe zone image
 var safeZoneImage = new Image();
